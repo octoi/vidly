867174dc09807d51816e5558e6f2a15409c734b5
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-04 07:09:06 +0530

    customizing admin

--- vidly/movies/admin.py
+++ vidly/movies/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Genre, Movie
 
-admin.site.register(Genre)
-admin.site.register(Movie)
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+class MovieAdmin(admin.ModelAdmin):
+    exclude = ('date_created',)
+    list_display = ('title', 'number_in_stock', 'daily_rate')
+
+
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Movie, MovieAdmin)
