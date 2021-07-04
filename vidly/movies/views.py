ed46b78ef910c0b508b1b620b8442305196c9231
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-04 10:19:38 +0530

    returning html file

--- vidly/movies/views.py
+++ vidly/movies/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 # Create your views here.
+
+
 def index(request):
-	return  HttpResponse("Hello World")
+    movies = Movie.objects.all()
+    return render(request, 'index.html', {'movies': movies})
