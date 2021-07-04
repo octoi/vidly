d5901d362fea0fde7122ddad7469d7ad469d44f2
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-04 06:52:42 +0530

    added date created

--- vidly/movies/models.py
+++ vidly/movies/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -13,3 +14,4 @@ class Movie(models.Model):
     number_in_stock = models.IntegerField()
     daily_rate = models.FloatField()
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(default=timezone.now)
