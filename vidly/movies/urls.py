7326d3290d28d7754e0406aba86a3cd401a6cf24
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-04 10:49:59 +0530

    movie id path

--- vidly/movies/urls.py
+++ vidly/movies/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='movies_index'),
+    path('<movie_id>', views.detail, name='movies_detail')
 ]
