5609ee52f33fc2aa42aa266c82adca5a8f5011d2
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-04 10:58:57 +0530

    404

--- vidly/movies/views.py
+++ vidly/movies/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Movie
 
 # Create your views here.
@@ -11,5 +11,5 @@ def index(request):
 
 
 def detail(request, movie_id):
-    movie = Movie.objects.get(pk=movie_id)
+    movie = get_object_or_404(Movie, pk=movie_id)
     return render(request, 'movies/detail.html', {'movie': movie})
